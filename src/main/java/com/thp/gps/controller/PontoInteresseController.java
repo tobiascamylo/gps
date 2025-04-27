09d424984dae21b14abb54487e1c9f07d1cb418d
Tobias <tobiascamylo@gmail.com>
2025-04-27 18:59:45 -0300

    create new path file PontoInteresseController

--- src/main/java/com/thp/gps/controller/PontoInteresseController.java
+++ src/main/java/com/thp/gps/controller/PontoInteresseController.java
@@ -4,10 +4,7 @@ import com.thp.gps.model.PontosInteresse;
 import com.thp.gps.model.PontosInteresseDTO;
 import com.thp.gps.repository.PontosInteresseRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +27,24 @@ public class PontoInteresseController {
     public ResponseEntity<List<PontosInteresse>> listarPontosInteresse(){
         List<PontosInteresse> listaInteresse = repository.findAll();
         return ResponseEntity.ok(listaInteresse);
+    }
+
+    @GetMapping("/listar-pontos-proximos")
+    public ResponseEntity<List<PontosInteresse>> listarPontosProximos(@RequestParam("x") Long x, @RequestParam("y") Long y,@RequestParam("dmax") Long dmax){
+
+        long xMin = x - dmax;
+        long xMax = x + dmax;
+        long yMin = y - dmax;
+        long yMax = y + dmax;
+        List<PontosInteresse>pontosFiltradosInteresse = repository.findPontosInteresseProximos(xMin, xMax, yMin, yMax)
+                .stream()
+                .filter(p -> distanciaEuclidiana(x,y, p.getX(), p.getY()) <= dmax)
+                .toList();
+
+        return ResponseEntity.ok(pontosFiltradosInteresse);
+    })
+
+    public double distanciaEuclidiana(long x1, long y1, long x2, long y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 }
