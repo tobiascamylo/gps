79de7761050149bda747b20d9f6fd914b901e553
Tobias <tobiascamylo@gmail.com>
2025-04-27 16:11:54 -0300

    add folders configurations

---
+++ src/test/java/com/thp/gps/GpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thp.gps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
