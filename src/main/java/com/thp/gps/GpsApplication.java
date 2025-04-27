7d1d95edeee621899bd7f0882a7501f949d74b64
Tobias <tobiascamylo@gmail.com>
2025-04-27 18:58:28 -0300

    configuration class aplication gps

--- src/main/java/com/thp/gps/GpsApplication.java
+++ src/main/java/com/thp/gps/GpsApplication.java
@@ -1,13 +1,39 @@
 package com.thp.gps;
 
+import com.thp.gps.model.PontosInteresse;
+import com.thp.gps.repository.PontosInteresseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GpsApplication {
+/**
+ * @author Tobias Correa
+ * @version 1.0
+ * @since 2025-04-27
+ *
+ * This is the main application class for the GPS application.
+ * It initializes the Spring Boot application and populates the database with sample data.
+ */
+public class GpsApplication implements CommandLineRunner {
+
+	@Autowired
+	private PontosInteresseRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GpsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		repository.save(new PontosInteresse("Lanchonete", 27L, 12L));
+		repository.save(new PontosInteresse("Posto", 31L, 18L));
+		repository.save(new PontosInteresse("Joalheria", 15L, 12L));
+		repository.save(new PontosInteresse("Floricultura", 19L, 21L));
+		repository.save(new PontosInteresse("Pub", 12L, 8L));
+		repository.save(new PontosInteresse("Supermercado", 23L, 6L));
+		repository.save(new PontosInteresse("Churrascaria", 28L, 2L));
+
+	}
 }
